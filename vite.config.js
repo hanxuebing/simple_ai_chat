613fa0f75c5db6ad73806402a89eb44ea03c7b84
hanxuebing <hanxuebing@360.cn>
2026-03-19 16:21:43 +0800

    first commit

---
+++ vite.config.js
@@ -0,0 +1,35 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import AutoImport from 'unplugin-auto-import/vite'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    Components({
+      globs: ['!src/components/**/*.vue'],
+      resolvers: [ElementPlusResolver()],
+    }),
+    AutoImport({
+      ignore: ['src/components/**'],
+      resolvers: [ElementPlusResolver()],
+      imports: ['vue', 'vue-router', 'pinia'],
+      eslintrc: {
+        enabled: true,
+        filepath: './.eslintrc-auto-import.json',
+      },
+    }),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+})
+
