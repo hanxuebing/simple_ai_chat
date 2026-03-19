6e676e4e9245d87a4bd4329609ca49f795b0c9d7
hanxuebing <hanxuebing@360.cn>
2026-03-19 19:18:10 +0800

    📦 build(vite): 配置开发服务器端口与代理
    - 引入 loadEnv 函数以支持读取环境变量
    - 配置 host 为 0.0.0.0 允许局域网访问
    - 从环境变量读取端口配置
    - 添加 API 代理配置解决开发环境跨域问题

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { loadEnv, defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
@@ -8,7 +8,8 @@ import AutoImport from 'unplugin-auto-import/vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
   return {
     plugins: [
       vue(),
@@ -33,6 +34,18 @@ export default defineConfig(() => {
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
+    },
+    server: {
+      host: '0.0.0.0', // 绑定到所有网络地址
+      port: +env.VITE_APP_PORT,
+      proxy: {
+        // 代理 /dev-api 的请求
+        [env.VITE_APP_BASE_API]: {
+          target: env.VITE_APP_API_URL,
+          // 修改代理服务器Origin为目标服务器target
+          changeOrigin: true,
+        },
       },
     },
   }
