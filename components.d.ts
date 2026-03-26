11eb80668a5856c3b96bfbc3c96ab912d0fe80a6
hanxuebing <hanxuebing@360.cn>
2026-03-26 11:44:39 +0800

    ✨ feat(components): 添加 ElDialog 组件类型声明
    - 新增 ElDialog 组件的全局类型声明

--- components.d.ts
+++ components.d.ts
@@ -12,6 +12,7 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     ElButton: typeof import('element-plus/es')['ElButton']
+    ElDialog: typeof import('element-plus/es')['ElDialog']
     ElEmpty: typeof import('element-plus/es')['ElEmpty']
     ElIcon: typeof import('element-plus/es')['ElIcon']
     ElPopover: typeof import('element-plus/es')['ElPopover']
